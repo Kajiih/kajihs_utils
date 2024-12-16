55a130c33e0cbf0712a26c5fd425a873b8202e2b
Julian Paquerot <111422357+Kajiih@users.noreply.github.com>
2024-12-16 23:24:29 +0100

    🚸 Improve auto_subplot

--- src/kajihs_utils/pyplot.py
+++ src/kajihs_utils/pyplot.py
@@ -12,7 +12,7 @@ from .arithmetic import (
 
 
 def auto_subplot(
-    size: int, /, ratio: float = 9 / 16, **subplot_params: Any
+    size: int, /, ratio: float = 9 / 16, portrait: bool = False, **subplot_params: Any
 ) -> tuple[Figure, ndarray[tuple[int], Any]]:
     """
     Automatically creates a subplot grid with an adequate number of rows and columns.
@@ -20,17 +20,25 @@ def auto_subplot(
     Args:
         size: The total number of subplots.
         ratio: The threshold aspect ratio between rows and columns.
+        portrait: Whether there should be more rows than columns instead of the
+            opposite
         **subplot_params: Additional keyword parameters for subplot.
 
     Returns:
         Tuple containing the figure and the flatten axes.
     """
-    rows, cols = almost_factors(size, ratio)
+    # Special case for 2
+    large, small = (2, 1) if size == 2 else almost_factors(size, ratio)  # noqa: PLR2004
+    rows, cols = (large, small) if portrait else (small, large)
 
     fig, axes = plt.subplots(rows, cols, **subplot_params)
 
     # if isinstance(axes, np.ndarray):
     #     axes = axes.flatten()
     axes = axes.flatten()
+
+    # Hide the remaining axes if there are more axes than subplots
+    for i in range(size, len(axes)):
+        axes[i].set_axis_off()
 
     return fig, axes
