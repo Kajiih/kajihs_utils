ed86a1e91f704376362ee0f07d37d0b2358b88cc
Julian Paquerot <111422357+Kajiih@users.noreply.github.com>
2025-01-06 14:21:18 +0100

    🐛 Fix and improve comparison protocols

--- src/kajihs_utils/protocols.py
+++ src/kajihs_utils/protocols.py
@@ -8,6 +8,37 @@ from typing import (
 
 @runtime_checkable
 class SupportsLessThan[T](Protocol):
-    """A protocol for objects supporting less-than comparisons."""
+    """Objects supporting less-than comparisons."""
 
-    def __lt__(self, other: T) -> bool: ...
+    def __lt__(self, __other: T) -> bool: ...
+
+
+@runtime_checkable
+class SupportsLessOrEqual[T](Protocol):
+    """Objects supporting less-or-equal comparisons."""
+
+    def __lt__(self, __other: T) -> bool: ...
+
+
+@runtime_checkable
+class SupportsGreaterThan[T](Protocol):
+    """Objects supporting greater-than comparisons."""
+
+    def __lt__(self, __other: T) -> bool: ...
+
+
+@runtime_checkable
+class SupportsGreaterOrEqual[T](Protocol):
+    """Objects supporting greater-or-equal comparisons."""
+
+    def __lt__(self, __other: T) -> bool: ...
+
+
+class SupportsAllComparisons[T](
+    SupportsLessThan[T],
+    SupportsLessOrEqual[T],
+    SupportsGreaterThan[T],
+    SupportsGreaterOrEqual[T],
+    Protocol,
+):
+    """Objects supporting all comparisons."""
